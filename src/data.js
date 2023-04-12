ed5d2eb3053968817512af7b5bc2aeee557d4520
Kat Connolly <hi.connkat@gmail.com>
2023-04-11 23:45:23 -0600

    Name correction

--- src/data.js
+++ src/data.js
@@ -91,6 +91,6 @@ export const rows = [
   createData(32,"June 14, 1992", "1992", "Birthday", "Kate", "HNJbrR_9SLw"),
   createData(33,"Dec 26, 1991", "1991", "First Steps", "Kate", "oednobezVFo"),
   createData(34,"Dec 1, 1990", "1990", "MSM", null, null, "noGg-JTWfZE/edit"),
-  createData(35,"Aug 1,1991", "1991", "Cameron Lake", null, "aRjkvvUTDbM"),
+  createData(35,"Aug 1,1991", "1991", "Stoney Lake", null, "aRjkvvUTDbM"),
   createData(36,"Nov 30, 1990", "1991", "MSM", "Kate", "s-1AbqM0Yp0"),
 ];
